3d00e30e8882d2b5366a23ae6845d6cec6edd28f
Md Aadil <mda231034@gmail.com>
2025-06-17 06:38:18 -0700

    Add files via upload

---
+++ portfolio/main/data/Data.js
@@ -0,0 +1,8 @@
+function data () {
+    const name = document.getElementById("naam").value;
+    const email = document.getElementById("email").value;
+    const messege = document.getElementById("message").value;
+   
+     
+}
+console.log(` Name :${name} Email: ${email} Messege :${messege}`)
